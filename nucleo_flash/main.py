090dbd9344557a1f53b20b8ded2f3dba05e8872e
Jolatomme <severin.trochut@libertysurf.fr>
2024-04-10 18:20:17 +0200

    MAX7219 testing

--- nucleo_flash/main.py
+++ nucleo_flash/main.py
@@ -1,9 +1,28 @@
 # main.py -- put your code here!
-# pyb and machine module already imported
+# pyb module already imported
+from machine import Pin, SPI
+import max7219
 import dcf77
 
+# Using SPI 2 for easy PIN wiring
+# clk : SCK PB13
+# din : MOSI PB15
+spi = SPI(2, baudrate=10000000)
+
+# Use a single 4x 8x8 matrix (32x8) pixels
+screen = max7219.Max7219(32, 8, spi, Pin('D7'))
+
+# Adjust screen brightness
+#  0: min
+# 15: MAX
+screen.brightness(0)
+
+# Clear screen
+screen.fill(0)
+screen.show()
+
 led = pyb.LED(1)
-dcf = dcf77.dcf77(machine.Pin('D4'))
+dcf = dcf77.dcf77(Pin('D4'))
 rtc = pyb.RTC()
 
 # Cutstom irq handler
@@ -17,12 +36,16 @@ def handler():
 dcf.irq([dcf.IRQ_MINUTE], handler)
 
 # Starting receiving and decoding
-dcf.start()
+#dcf.start()
 
 # Wait for valid signal
-while not dcf.get_Infos()['Valid']:
-    pass
+#while not dcf.get_Infos()['Valid']:
+#    pass
 
+# Datetime structure
+# [4] : H
+# [5] : min.
+# [6] : sec.
 # Initializing loop
 prev_sec = 0
 
@@ -30,6 +53,10 @@ prev_sec = 0
 while (True):
     sec = rtc.datetime()[6]
     if (sec != prev_sec):
+        sec_txt = '{0:0=2d}'.format(sec)
+        screen.fill(0)
+        screen.text(sec_txt, 8, 0, 1)
+        screen.show()
         if sec in (50, 55, 56, 57, 58, 59):
             led.on()
             pyb.delay(200)
